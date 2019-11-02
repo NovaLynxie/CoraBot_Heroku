e3c491663e48d276ad8dcc6f6e6b1698299beb71
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-02 17:19:53 +0000

    Rewrite of the music commands
    This applies the new command format into the music command files.
    I have a feeling the 'play' command might need some tweaks for it to work correctly if something breaks...

--- cora_modules/cora.cmds/music/skip.js
+++ cora_modules/cora.cmds/music/skip.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(message){
       const serverQueue = message.client.queue.get(message.guild.id);
       if (!message.member.voiceChannel) {
-        console.log("[CoraBot] Music.Skip Error! UserNotFound_voicechat.channelNoUser") 
+        console.log("[CoraBot] Music.Skip Error! UserNotFound_voicechat.channelNoUser")
         return message.channel.send('You have to be in a voice channel to skip the music!')
       }
       if (!serverQueue) {
@@ -18,4 +18,29 @@ module.exports = {
       serverQueue.connection.dispatcher.end();
       message.channel.send(":arrow_forward: Song Skipped!")
     }
+};
+
+module.exports.run = async (bot, message, args) => {
+  const serverQueue = message.client.queue.get(message.guild.id);
+  if (!message.member.voiceChannel) {
+    console.log("[CoraBot] Music.Skip Error! UserNotFound_voicechat.channelNoUser")
+    return message.channel.send('You have to be in a voice channel to skip the music!')
+  }
+  if (!serverQueue) {
+    console.log("[CoraBot] Music.Skip Error! SkipErr_voicechat.channelQueue.Skip")
+    return message.channel.send('There is no song that I could skip!');
+  }
+  serverQueue.connection.dispatcher.end();
+  message.channel.send(":arrow_forward: Song Skipped!")
+};
+
+// Help Object
+module.exports.help = {
+  name: "skip",
+  description: "Skips the currently playing song.",
+  usage: "skip",
+  category: "music",
+  aliases: [""]
+  cooldown: 3,
+  guildOnly true
 };
--- cora_modules/cora.cmds/music/stop.js
+++ cora_modules/cora.cmds/music/stop.js
@@ -18,3 +18,25 @@ module.exports = {
         message.channel.send(":octagonal_sign: Music Stopped!")
     }
 };
+
+module.exports.run = async (bot, message, args) => {
+  const serverQueue = message.client.queue.get(message.guild.id);
+  if (!message.member.voiceChannel) {
+      console.log("[CoraBot] Music.Stop Error! UserNotFound_voicechat.channelNoUser")
+      return message.channel.send('You have to be in a voice channel to stop the music!')};
+  if (!serverQueue) {
+      console.log("[CoraBot] Music.Stop Error! StopErr_voicechat.channelQueue.Stop")
+      return message.channel.send("Nothing is playing. No songs in queue.");
+  }
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+  message.channel.send(":octagonal_sign: Music Stopped!")
+};
+
+module.exports.help = {
+  name: "stop",
+  description: "Stops the currently playing song and disconnects the bot from vc channel.",
+  usage: "stop",
+  category: "music",
+  aliases: ["finish", "end"]
+};
