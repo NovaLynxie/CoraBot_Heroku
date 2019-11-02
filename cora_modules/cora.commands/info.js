3164a6041567a1d2e989e28fb4dfb178957450ea
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-02 10:38:16 +0000

    Update info.js
    Added larger description... hopefully doesn't hit the discord character limit >.>

--- cora_modules/cora.commands/info.js
+++ cora_modules/cora.commands/info.js
@@ -24,7 +24,7 @@ module.exports = {
                 .setThumbnail(bot.user.avatarURL)
                 .addField("Name & TagID:",bot.user.username+' ('+bot.user.tag+')')
                 .addField("Created:",bot.user.createdAt)
-                .addField("About Me", "I am Nova's Personal bot. I am mostly used for testing features and stuff. Sometimes I play music but not that well... I do try though ^w^") 
+                .addField("About Me", "Hi there! My name is Cora. I am NovaLynxie's personal discord bot built using discord.js on nodejs. A while back, I was made as a small test bot to help run experimental features and help them as a discord bot for their guild. Now, I am much more stable compared to my early days of running experimental code and testing the limits of what I could achieve. My main purpose is to help you bring some life to your discord guild or community server, wherever I can. I can play some music for you, give you information about me or help you moderate your own guild using my own custom built admin commands! Don't worry, I also come with a help command so you can find out what commands I have, just ask whenever you're having trouble remembering one of my commands.") 
                 .setFooter("Created by NovaLynxie#9765, coded in Discord.JS v11.5.1, coded in Discord.JS v11.5.1")
             message.channel.send(embed);
             return;
@@ -49,7 +49,7 @@ module.exports = {
                 .addField("Did you enter the command correctly?", "Check you entered the command correctly, \nCommand usage is `>info <args> [bot, owner]`.")
                 .setFooter("Created by NovaLynxie#9765, coded in Discord.JS v11.5.1")
             message.channel.send(embed);
-            return; 
+            return;
         }
     }
 };
