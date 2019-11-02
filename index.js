28331abc9194bc358869b3063a00f1a632135d57
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-02 13:41:54 +0000

    Command handler rewrite v2.0?
    Rewrite of command handler... again. This should allow it to read subdirectories and allow a user to organise the commands without putting them into one massive bundle under cora.cmds

--- index.js
+++ index.js
@@ -18,6 +18,43 @@ const cooldowns = new Discord.Collection();
 //var err = fs.createWriteStream(logDir+'/corabot.error.log')
 
 // Command files handler to parse <cmd>.js files.
+const loadCmds = (dir = "./commands/") => {
+
+	readdirSync(dir).forEach(dirs => {
+	// we read the commands directory for sub folders and filter the files with name with extension .js
+		const commands = readdirSync(`${dir}${sep}${dirs}${sep}`).filter(files => files.endsWith(".js"));
+
+		// we use for loop in order to get all the commands in sub directory
+		for (const file of commands) {
+		// We make a pull to that file so we can add it the bot.commands collection
+			const pull = require(`${dir}/${dirs}/${file}`);
+			// we check here if the command name or command category is a string or not or check if they exist
+			if (pull.help && typeof (pull.help.name) === "string" && typeof (pull.help.category) === "string") {
+				if (bot.commands.get(pull.help.name)) return console.warn(`${warning} Two or more commands have the same name ${pull.help.name}.`);
+				// we add the the comamnd to the collection, Map.prototype.set() for more info
+				bot.commands.set(pull.help.name, pull);
+				// we log if the command was loaded.
+				console.log(`${success} Loaded command ${pull.help.name}.`);
+			}
+			else {
+			// we check if the command is loaded else throw a error saying there was command it didn't load
+				console.log(`${error} Error loading command in ${dir}${dirs}. you have a missing help.name or help.name is not a string. or you have a missing help.category or help.category is not a string`);
+				// we use continue to load other commands or else it will stop here
+				continue;
+			}
+			// we check if the command has aliases, is so we add it to the collection
+			if (pull.help.aliases && typeof (pull.help.aliases) === "object") {
+				pull.help.aliases.forEach(alias => {
+					// we check if there is a conflict with any other aliases which have same name
+					if (bot.aliases.get(alias)) return console.warn(`${warning} Two commands or more commands have the same aliases ${alias}`);
+					bot.aliases.set(alias, pull.help.name);
+				});
+			}
+		}
+	});
+};
+loadCmds();
+/*
 const cmdsDir = './cora_modules/cora.commands/'
 const cmdsData = fs.readdirSync(cmdsDir).filter(cmdsFile => cmdsFile.endsWith('.js'));
 console.log("[System] Searching for files from `"+cmdsDir+"`. Please wait...")
@@ -25,10 +62,10 @@ console.log("[System] Fetching commands from `"+cmdsDir+"` and storing into comm
 for (const cmdsFile of cmdsData) {
   const cmds = require(cmdsDir+`${cmdsFile}`)
   bot.commands.set(cmds.name, cmds)
-  if (debug === true) {console.log("[Debug] Added "+cmdsFile+" successfully!")} //Debug console prompt to confirm command file is validated.
+  if (debug === true) {console.log("[Debug] Loaded "+cmdsFile+" successfully!")} //Debug console prompt to confirm command file is validated.
 
 }
-
+*/
 // Verbose console log debugger. To enable prompts, set debug in config.json to true.
 if (debug === true) {
   console.log("[Debug] Dev Mode Active! Verbose logging enabled.");
