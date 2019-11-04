86afffbaa0427e5ff1c91dcf27fedad2b0155013
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-04 12:44:45 +0000

    Redoing the command handler rewrite... again.
    The current command handler rewrite was too much trouble to get it working with cora's existing command setup, so I'm ditching it and trying another approach instead.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Loads required modules into the code.
 const Discord = require("discord.js");
-// Links code to other required parts.
+// Links code to other required parts needed in code.
 const fs = require('fs');
-// Destructing methods used to define lines in code.
-const { Client, Collection } = require("discord.js");
-const { readdirSync } = require("fs");
-const { sep } = require("path");
-const { success, error, warning } = require("log-symbols"); // npm i log-symbols or yarn add log-symbols
-// Loads config to allow for usage.
+// Loads required variables into code.
+const modules = ['administration','development','economy','moderation','music','utility'];
 const token = process.env.token
 const {
   prefix,
@@ -16,63 +12,25 @@ const {
 
 // Variables for DiscordBot
 const bot = new Client();
-["commands", "aliases"].forEach(x => bot[x] = new Collection());
-const cooldowns = new Discord.Collection();
+const dir = `./cora_modules/cora.cmds/`
+
+modules.forEach(c => {
+  fs.readdir(dir+`${c}/`, (err, files) => {
+    if (err) throw err;
+    console.log(`[CmdLogs] Loaded ${files.length} commands of module ${c}`)
+    files.forEach(f => {
+      const props = require(dir+`${c}/${f}`)
+      bot.commands.set(props.help.name, props);
+      props.conf.aliases.forEach(alias => {
+        bot.aliases.set(alias, props.name)
+      })
+    })
+  })
+})
+
 //const logDir = './cora_modules/cora.debug/'
 //var sys = fs.createWriteStream(logDir+'/corabot.access.log')
 //var err = fs.createWriteStream(logDir+'/corabot.error.log')
-
-// Command files handler to parse <cmd>.js files.
-// Made by Anish Shobith, modified for use with CoraBot.
-const loadCmds = (dir = "./cora_modules/cora.cmds/") => {
-
-	readdirSync(dir).forEach(dirs => {
-	// we read the commands directory for sub folders and filter the files with name with extension .js
-		const commands = readdirSync(`${dir}${sep}${dirs}${sep}`).filter(files => files.endsWith(".js"));
-
-		// we use for loop in order to get all the commands in sub directory
-    for (const file of commands) {
-		// We make a pull to that file so we can add it the bot.commands collection
-			const pull = require(`${dir}/${dirs}/${file}`);
-			// we check here if the command name or command category is a string or not or check if they exist
-			if (pull.help && typeof (pull.help.name) === "string" && typeof (pull.help.category) === "string") {
-				if (bot.commands.get(pull.help.name)) return console.warn(`${warning} Two or more commands have the same name ${pull.help.name}.`);
-				// we add the the comamnd to the collection, Map.prototype.set() for more info
-				bot.commands.set(pull.help.name, pull);
-				// we log if the command was loaded.
-				console.log(`${success} Loaded command ${pull.help.name}.`);
-
-			}
-			else {
-			// we check if the command is loaded else throw a error saying there was command it didn't load
-				console.log(`${error} Error loading command in ${dir}${dirs}. you have a missing help.name or help.name is not a string. or you have a missing help.category or help.category is not a string`);
-				// we use continue to load other commands or else it will stop here
-				continue;
-			}
-			// we check if the command has aliases, is so we add it to the collection
-			if (pull.help.aliases && typeof (pull.help.aliases) === "object") {
-				pull.help.aliases.forEach(alias => {
-					// we check if there is a conflict with any other aliases which have same name
-					if (bot.aliases.get(alias)) return console.warn(`${warning} Two commands or more commands have the same aliases ${alias}`);
-					bot.aliases.set(alias, pull.help.name);
-				});
-			}
-		}
-	});
-};
-loadCmds();
-/* Depreciated command handler */
-/*
-const cmdsDir = './cora_modules/cora.commands/'
-const cmdsData = fs.readdirSync(cmdsDir).filter(cmdsFile => cmdsFile.endsWith('.js'));
-console.log("[System] Searching for files from `"+cmdsDir+"`. Please wait...")
-console.log("[System] Fetching commands from `"+cmdsDir+"` and storing into commands table...")
-for (const cmdsFile of cmdsData) {
-  const cmds = require(cmdsDir+`${cmdsFile}`)
-  bot.commands.set(cmds.name, cmds)
-  if (debug === true) {console.log("[Debug] Loaded "+cmdsFile+" successfully!")} //Debug console prompt to confirm command file is validated.
-}
-*/
 
 // Verbose console log debugger. To enable prompts, set debug in config.json to true.
 if (debug === true) {
