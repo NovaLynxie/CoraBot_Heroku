0416b7568e7e9117eda1e5ec3dc6d49208dba48f
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-11 16:25:33 +0000

    Update poke.js
    Forgot to define Command in the poke command file. This fixes it.

--- cora_commands/fun/poke.js
+++ cora_commands/fun/poke.js
@@ -1,8 +1,9 @@
+const {Command} = require('discord.js-commando')
 module.exports = class PokeCommand extends Command {
   constructor(bot) {
     super(bot, {
-        name: '',
-        aliases: [''],
+        name: 'poke',
+        aliases: ['nudge'],
         group: 'fun',
         memberName: 'poke',
         description: "Gives Cora a small nudge. `Cora: 'Hey that tickles! xD'`",
@@ -17,13 +18,3 @@ module.exports = class PokeCommand extends Command {
     message.channel.send("<a:pawingcat:635163464905523221> "+message.author.toString())
   }
 }
-
-module.exports.help = {
-  name: "poke",
-  description: "Gives Cora a small nudge. `Cora: 'Hey that tickles! xD'`",
-  aliases: ["tap", "nudge"],
-  usage: "poke",
-  category: "utility",
-  cooldown: 5,
-  guildOnly: false
-};
