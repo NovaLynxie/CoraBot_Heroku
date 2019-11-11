a4d8de7da4ed1670a977406a136fa091d898e466
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-11 11:54:45 +0000

    Update index.js
    Commando Update rewrite.

--- index.js
+++ index.js
@@ -7,5 +7,26 @@ const {
 const bot = new CommandoClient({
   commandPrefix: '>',
   owner: [''],
+  //owner: process.env.owners, (enable when hosting online)
   //invite: '',
 })
+
+bot.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['administration', '']
+    ['development', '']
+    ['moderation', '']
+    ['music', '']
+    ['utility', '']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'cora_commands'));
+
+bot.once('ready', () => {
+  console.log(`Connected to DISCORD as ${bot.user.tag} (${bot.user.id})`);
+  bot.user.setActivity("the guild.", {type:"WATCHING"})
+})
+
+bot.login(process.env.token); //Use environment variable token to hide bot token.
