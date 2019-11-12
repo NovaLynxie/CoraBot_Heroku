10961292f0b4db6092952f92a2855b15e012125d
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-12 08:49:07 +0000

    Fix missing function definitions.
    Seems I might have forgotten to add these required functions for the command to play properly. Added them in so play.js shouldn't crash.

--- cora_commands/music/play.js
+++ cora_commands/music/play.js
@@ -19,6 +19,9 @@ module.exports = class PlayCommand extends Command {
         })
     }
     async run(message, args) {
+		const searchString = args.slice(1).join(' ');
+		const url = args[0] ? args[0].replace(/<(.+)>/g, '$1') : '';
+
         const voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
 		const permissions = voiceChannel.permissionsFor(message.client.user);
@@ -71,5 +74,67 @@ Please provide a value to select one of the search results ranging from 1-10.
 			}
 			return handleVideo(video, message, voiceChannel);
 		}
+	};
+
+	async handleVideo(video, msg, voiceChannel, playlist = false) {
+		const serverQueue = queue.get(msg.guild.id);
+		console.log(video);
+		const song = {
+			id: video.id,
+			title: Util.escapeMarkdown(video.title),
+			url: `https://www.youtube.com/watch?v=${video.id}`
+		};
+		if (!serverQueue) {
+			const queueConstruct = {
+				textChannel: msg.channel,
+				voiceChannel: voiceChannel,
+				connection: null,
+				songs: [],
+				volume: 2,
+				playing: true
+			};
+			queue.set(msg.guild.id, queueConstruct);
+	
+			queueConstruct.songs.push(song);
+	
+			try {
+				var connection = await voiceChannel.join();
+				queueConstruct.connection = connection;
+				play(msg.guild, queueConstruct.songs[0]);
+			} catch (error) {
+				console.error(`I could not join the voice channel: ${error}`);
+				queue.delete(msg.guild.id);
+				return msg.channel.send(`I could not join the voice channel: ${error}`);
+			}
+		} else {
+			serverQueue.songs.push(song);
+			console.log(serverQueue.songs);
+			if (playlist) return undefined;
+			else return msg.channel.send(`✅ **${song.title}** has been added to the queue!`);
+		}
+		return undefined;
+	}
+	
+	play(guild, song) {
+		const serverQueue = queue.get(guild.id);
+	
+		if (!song) {
+			serverQueue.voiceChannel.leave();
+			queue.delete(guild.id);
+			return;
+		}
+		console.log(serverQueue.songs);
+	
+		const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+			.on('end', reason => {
+				if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+				else console.log(reason);
+				serverQueue.songs.shift();
+				play(guild, serverQueue.songs[0]);
+			})
+			.on('error', error => console.error(error));
+		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+	
+		serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
 	}
 };
