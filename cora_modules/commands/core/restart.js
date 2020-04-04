933dacafbdb404dcf625d7e07b8d81a6b18fe6a8
Macky Fitz <pixelgamerx32@gmail.com>
2020-04-04 13:46:46 +0100

    Adding new changes from commando-dev
    Adds a fully working restart command!

--- cora_modules/commands/core/restart.js
+++ cora_modules/commands/core/restart.js
@@ -1,5 +1,4 @@
 const { Command } = require('discord.js-commando');
-
 module.exports = class RestartCommand extends Command {
     constructor(client) {
         super(client, {
@@ -12,7 +11,14 @@ module.exports = class RestartCommand extends Command {
         });
     }
     run(message) {
-        console.log('[Bot] This is a placeholder, feature not yet implemented.')
-        message.say("I'm sorry, but this feature is not yet implemented. :(")
+        console.log("[Cora] Restart command received!")
+        console.log("[Warn] Status may be out of sync for a few minutes.")
+        //message.channel.send("Good night... <:sleepycat:635163563878514688>")
+        message.say("Will be right back.")
+            .then(console.log("[Cora] Restarting my systems..."))
+            .then(_msg => this.client.destroy())
+            .then(_msg => this.client.login(process.env.botToken))
+            .then(console.log("[Cora] Restart completed successfully!"))
+            .then(_msg => this.client.user.setActivity('with Commando'));
     }
 }
