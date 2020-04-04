4c95565a737833f6e4a0ec70329d9ecf541b299e
Macky Fitz <pixelgamerx32@gmail.com>
2020-04-04 12:12:16 +0100

    Update statsinfo.js
    Updating the status information command.

--- cora_modules/commands/info/statsinfo.js
+++ cora_modules/commands/info/statsinfo.js
@@ -1,5 +1,9 @@
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const { stripIndents } = require('common-tags');
+const { version } = require('../../package')
 module.exports = class BotInfoCommand extends Command {
     constructor(client) {
         super(client, {
@@ -11,12 +15,37 @@ module.exports = class BotInfoCommand extends Command {
         });
     }
     run(message) {
+        const servers = this.client.guilds.cache.size
+        const users = this.client.users.cache.filter(user => !user.bot).size
         const embed = new MessageEmbed()
             .setTitle("Bot Statistics")
             .setColor(0x154360)
-            .addField("Servers", "Connected to " + this.client.guilds.cache.size + " servers")
-            .addField("Users", "Managing x users" )
-            .setFooter("Built on Node.js using Discord.js with Commando.")
+            .addFields(
+                
+                {
+                    name: '> Total Uptime',
+                    value: moment.duration(this.client.uptime)
+                            .format('d[ days], h[ hours], m[ minutes ]s[ seconds]'),
+                    inline: true
+                },
+                {
+                    name: '> Memory Usage',
+                    value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`
+                },
+                {
+                    name: '> General Stats',
+                    value: stripIndents`
+                    Guilds: ${this.client.guilds.cache.size} servers
+                    Channels: ${this.client.channels.cache.size} channels
+                    Users: ${this.client.users.cache.filter(user => !user.bot).size} users
+                    `
+                },
+                {
+                    name: '> Version',
+                    value: `v${version}`,
+                    inline: true
+                }
+            )
             .setThumbnail(this.client.user.displayAvatarURL({ formant: 'png'}))
         return message.embed(embed);
     }
