3148077059e586c5201cec1f537bbb364740fdc0
Macky Fitz <pixelgamerx32@gmail.com>
2020-04-04 11:06:59 +0100

    Update app.js
    Changing parts of the core code in app.js to adjust for changes.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { CommandoClient } = require('discord.js-commando');
 const { Structures } = require('discord.js');
 const path = require('path');
 
+const { activitiesList } = require('./zeon_modules/internal/activities.json');
 const prefix = process.env.prefix
 const botToken = process.env.token
 
@@ -23,36 +24,64 @@ Structures.extend('Guild', Guild => {
     return MusicGuild;
 });
 
-const bot = new CommandoClient({
+const client = new CommandoClient({
     commandPrefix: prefix,
-    owner: '234356998836191232',
+    owner: ownerID,
     invite: '',
 });
 
-bot.registry
+client.registry
     .registerDefaultTypes()
     .registerGroups([
         ['admin', 'Admin'],
         ['core', 'Core'],
         ['dev', 'Developer'],
         ['info', 'Information'],
-        ['misc', 'Random'],
+        ['misc', 'Miscelaneous'],
         ['music', 'Music'],
-        ['utils', 'Utility'],
-
     ])
     .registerDefaultGroups()
     .registerDefaultCommands({
-        eval: false,
         //help: false,
     })
-    .registerCommandsIn(path.join(__dirname, 'cora_cmds'));
+    .registerCommandsIn(
+        path.join(__dirname, './cora_modules/commands')
+    );
 
-bot.once('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}! (${bot.user.id})`);
-    bot.user.setActivity('with Commando');
+client.once('ready', () => {
+    console.log(`[Cora] Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('with Commando');
 });
 
-bot.on('error', console.error);
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activitiesList.length - 1) + 1);
+        if (index >= 0 && index <= 1) {
+            var statusType = 1 // 1 - Playing
+        };
+        if (index >= 2 && index <= 3) {
+            var statusType = 2 // 2 - Listening
+        };
+        if (index >= 4 && index <= 5) {
+        var statusType = 3 // 3 - Watching
+        };
+        client.user.setActivity(activitiesList[index], {type: statusType});
+    }, 300000);
+})
 
-bot.login(botToken);
+process.on('unhandledRejection', error => {
+    console.error('[NodeJS] Uncaught Promise Rejection!', error)
+})
+
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+    const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+    if (removedRoles.size > 0) console.log(`[Cora] Role ${removedRoles.map(r=>r.name)} removed from ${oldMember.displayName}.`)
+    const addedRoles = newMember.roles.cache.filter(role=>!oldMember.roles.cache.has(role.id));
+    if (addedRoles.size > 0) console.log(`[Cora] Role ${addedRoles.map(r=>r.name)} added to ${oldMember.displayName}.`)
+})
+
+client.on('error', error => {
+    console.error('[Zeon]', error)
+})
+
+client.login(botToken); //disabled as replaced with a cloud config loader.
