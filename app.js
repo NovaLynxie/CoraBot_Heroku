6dd5555338bd73654d13489d9364f88aa6828e37
Macky Fitz <pixelgamerx32@gmail.com>
2020-04-05 14:04:09 +0100

    Update app.js
    Disabling sqlite code and providers for now as this either does not work or throws errors on load of filename not defined.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { CommandoClient, SQLiteProvider } = require('discord.js-commando');
+const { CommandoClient, /*SQLiteProvider*/ } = require('discord.js-commando');
 const { Structures } = require('discord.js');
-const sqlite = require('sqlite');
+//const sqlite = require('sqlite');
 const path = require('path');
 console.log('[Init] Loading activity list providers...')
 const { activitiesList } = require('./cora_modules/internal/activities.json');
@@ -47,11 +47,6 @@ client.registry
     .registerCommandsIn(
         path.join(__dirname, './cora_modules/commands')
     );
-
-sqlite.open(path.join(__dirname, "settings.sqlite3")).then((db) => {
-    console.log('[SQLite] Loading settings database...')
-    client.setProvider(new SQLiteProvider(db));
-});
 
 client.once('ready', () => {
     console.log(`[Cora] Logged in as ${client.user.tag}! (${client.user.id})`);
