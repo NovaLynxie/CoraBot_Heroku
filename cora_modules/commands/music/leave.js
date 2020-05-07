99c6656faec83517a7e6e498e37915108cfb337c
Macky Fitz <pixelgamerx32@gmail.com>
2020-05-07 21:20:55 +0100

    Tweaks for streaming direct urls.
    Tweaks made to some music commands for stream feature to work properly.
    Leave command now actually kicks the bot from the voice channel instead of erroring out.
    Volume command now allows changing the volume based on guild commands.

--- cora_modules/commands/music/leave.js
+++ cora_modules/commands/music/leave.js
@@ -1,10 +1,11 @@
 const { Command } = require('discord.js-commando');
+const logger = require('../../providers/WinstonPlugin');
 
 module.exports = class LeaveCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'leave',
-      aliases: ['end'],
+      aliases: ['end','dc'],
       group: 'music',
       memberName: 'leave',
       guildOnly: true,
@@ -15,17 +16,29 @@ module.exports = class LeaveCommand extends Command {
   run(message) {
     var voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.reply('Join a channel and try again');
-
     if (
-      typeof message.guild.musicData.songDispatcher == 'undefined' ||
-      message.guild.musicData.songDispatcher == null
+      (typeof message.guild.musicData.radioDispatcher == 'undefined' ||
+      message.guild.musicData.radioDispatcher == null) &&
+      (typeof message.guild.musicData.songDispatcher == 'undefined' ||
+      message.guild.musicData.songDispatcher == null)
     ) {
-      return message.reply('There is no song playing right now!');
+      logger.warn('No currently running streams detected, aborting command.');
+      return message.reply('There is no song or radio playing right now!');
     }
-    if (!message.guild.musicData.queue)
-      return message.say('There are no songs in queue');
-    message.guild.musicData.songDispatcher.end();
-    message.guild.musicData.queue.length = 0;
+    if (!message.guild.musicData.queue) {
+      logger.warn('Nothing detected in the server queue, aborting command.')
+      return message.say('There are no songs in queue')
+    };
+    if (message.guild.musicData.radioDispatcher) {
+      message.guild.musicData.radioDispatcher.end();
+      logger.info('Stopping connected radio streams.')
+    }
+    if (message.guild.musicData.songDispatcher) {
+      message.guild.musicData.queue.length = 0;
+      message.guild.musicData.songDispatcher.end();
+      logger.info('Stopping current music playback.')
+    }
+    message.say('Finished playing 👋')
     return;
   }
 };
